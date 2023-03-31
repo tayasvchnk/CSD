273e1aca0bf6c6939a4879a98d6926063a61a87f
Taya <tkhurduikova@gmail.com>
2023-03-31 20:39:40 +0600

    Создала категории

---
+++ CSD.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,65 @@
+﻿using CSD.Web.Data;
+using CSD.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CSD.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly CSDContext _db;
+
+        public CategoriesController(CSDContext db)
+
+        {
+
+            _db = db;
+
+        }
+
+        public ActionResult Index()
+
+        {
+
+            IEnumerable<Category> categories = _db.Categories.ToList();
+
+            return View(categories);
+
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.FirstOrDefault(m => m.Id == id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+        // GET: Categories/Create 
+
+        public IActionResult Create()
+        {
+            ViewBag.Categories = new SelectList(_db.Categories.Where(x => x.ParentId == 0), "Id", "Name");
+            return View();
+        }
+        // POST: Categories/Create 
+
+        [HttpPost]
+
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+    }
+}
--- CSD.Web/Migrations/20230331140047_Add Category and Post tables.Designer.cs
+++ CSD.Web/Migrations/20230331140047_Add Category and Post tables.Designer.cs
@@ -4,6 +4,7 @@ using CSD.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CSD.Web.Migrations
 {
     [DbContext(typeof(CSDContext))]
-    partial class CSDContextModelSnapshot : ModelSnapshot
+    [Migration("20230331140047_Add Category and Post tables")]
+    partial class AddCategoryandPosttables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
