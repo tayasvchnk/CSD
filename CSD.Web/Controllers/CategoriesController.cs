71d0085d16a13c2f1d9d1dc7dd1f0296c5f65bed
Taya <tkhurduikova@gmail.com>
2023-03-31 20:58:30 +0600

    Добавила удаление категории

--- CSD.Web/Controllers/CategoriesController.cs
+++ CSD.Web/Controllers/CategoriesController.cs
@@ -62,7 +62,6 @@ namespace CSD.Web.Controllers
             return View(category);
         }
         // GET: Categories/Edit/5 
-
         public ActionResult Edit(int id)
         {
             var category = _db.Categories.Find(id);
@@ -70,12 +69,12 @@ namespace CSD.Web.Controllers
             {
                 return NotFound();
             }
+            ViewBag.Categories = new SelectList(_db.Categories.Where(x => x.ParentId == 0), "Id", "Name");
             return View(category);
         }
         // POST: Categories/Edit/5 
 
         [HttpPost]
-
         public IActionResult Edit(int id, Category category)
         {
             if (id != category.Id)
@@ -88,8 +87,32 @@ namespace CSD.Web.Controllers
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(category);
             ViewBag.Categories = new SelectList(_db.Categories.Where(x => x.ParentId == 0), "Id", "Name");
+            return View(category);
+            
+        }
+
+        // GET: Categories/Delete/5 
+        public IActionResult Delete(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        // POST: Categories/DeleteConfirmed/5 
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var category = _db.Categories.Find(id);
+            if (category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
